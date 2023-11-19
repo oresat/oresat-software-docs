cf6e038fe92b34b865647802355e50f3bf5967e2
ryanpdx <rmedick@protonmail.com>
2023-11-19 15:45:24 -0800

    autoformatted code with black and isort

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,4 @@
-'''Configuration file for the Sphinx documentation builder.'''
+"""Configuration file for the Sphinx documentation builder."""
 
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
@@ -8,32 +8,32 @@ from datetime import datetime
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'OreSat Software'
-copyright = f'{datetime.now().year}, Portland State Aerospace Society'
-author = 'PSAS'
-release = '1.0'
+project = "OreSat Software"
+copyright = f"{datetime.now().year}, Portland State Aerospace Society"
+author = "PSAS"
+release = "1.0"
 
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinxcontrib.mermaid',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.coverage',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.autodoc'
+    "sphinxcontrib.mermaid",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.coverage",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.autodoc",
 ]
 
 templates_path = []
-exclude_patterns = ['build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 html_domain_indices = True
 # html_static_path = ['static']
 
@@ -42,8 +42,8 @@ html_domain_indices = True
 
 # Example configuration for intersphinx: refer to the Python standard library.
 # To add links to stand python type definitions.
-intersphinx_mapping = {'python': ('https://docs.python.org/3/', None)}
+intersphinx_mapping = {"python": ("https://docs.python.org/3/", None)}
 
 
 def setup(app):
-    app.add_js_file('zoom.js')
+    app.add_js_file("zoom.js")
--- examples/app/app.py
+++ examples/app/app.py
@@ -2,14 +2,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 # The route() function of the Flask class is a decorator,
 # which tells the application which URL should call
 # the associated function.
-@app.route('/')
+@app.route("/")
 def hello_world():
     # ‘/’ URL is bound with hello_world() function.
-    return 'Hello, World!'
+    return "Hello, World!"
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
